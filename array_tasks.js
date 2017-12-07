e2930184ea492db46a1c757a0082923bdbd88c63
graeme doran <graeme42@icloud.com>
2017-12-07 09:14:22 +0000

    first test

---
+++ array_tasks.js
@@ -0,0 +1,39 @@
+const arrayTasks = {
+
+	concat: function (arr1, arr2) {
+     const new_array = arr1.concat(arr2);
+		 return new_array
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+	// 	const new_array = insertAt.(3,2);
+	// 	return new_array
+	// },
+
+	// square: function (arr) {
+
+	// },
+
+	// sum: function (arr) {
+
+	// },
+
+	// findDuplicates: function (arr) {
+
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+
+	// }
+
+}
+
+module.exports = arrayTasks
